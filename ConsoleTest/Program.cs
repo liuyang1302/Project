a4035442f2f23ea459964e37b5c1e1f0b60e032d
yang <yang@MODERN-LY>
2020-02-10 13:32:35 +0800

    添加项目文件。

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("测试GitHub");
+            Console.WriteLine("任意键关闭程序！");
+            int i = Console.Read();
+            if (i > 0)
+            {
+                
+            }
+        }
+    }
+}
